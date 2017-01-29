876d28629965c68efb4ffb425034fb413679c048
Micah M <mcmxl@reagan.com>
2017-01-29 12:19:19 -0700

    Update PseudoBid.py

--- PseudoBid.py
+++ PseudoBid.py
@@ -20,18 +20,27 @@
 	#View available bid lines.
 		#Display database of all existing bid lines.
 	
-	#View current bid assignments
+	#View current bid assignments.
+		#List choices
+			#By employee
+			#By bid line
 	
 	#Edit bid lines.
-		#Change existing bidlines.
-		#Add new bid lines.
-		#Delete bid lines.
+		#List choices
+			#Change existing bidlines.
+			#Add new bid lines.
+			#Delete bid lines.
 	
 	#Edit Employees.
-		#Change existing employee.
-		#Add new employee.
-		#Delete employee.
+		#List choices
+			#Change existing employee.
+			#Add new employee.
+			#Delete employee.
 		
 	#Have employee enter their name and seniority number.
 		#check employee is next in line by seniority number.
 		#List available bid lines and employee's current bid line.
+		
+	#Have employee enter new bid line choice.
+		#If chosen bid line is available assign it to the employee and make it unavailable to others.
+		#If chosen bidline is not available have employee choose another one.
