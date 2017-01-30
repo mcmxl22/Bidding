40433b6c8b1e654e744b0d5ee54db9b0caca1b67
Micah M <mcmxl@reagan.com>
2017-01-29 22:58:15 -0700

    Add files via upload

---
+++ PseudoBid.py
@@ -0,0 +1,47 @@
+#By Micah M. 2017
+
+#Create a database of all employees with their seniority numbers and current bid line numbers.
+
+#Create a database of all existing bid lines.
+	
+#Create an interface to allow employees to bid on the bid lines starting with the highest seniority.
+	#List activity choices.
+		#New Bid
+		#View available bid lines
+		#View current bid assignments
+		#Edit bid lines
+		#Edit Employees
+	
+	#New bid
+		#Ask if a new bid is being started.
+			#If yes proceed.
+			#If no return to activity list.
+			
+	#View available bid lines.
+		#Display database of all existing bid lines.
+	
+	#View current bid assignments.
+		#List choices.
+			#By employee
+			#By bid line
+	
+	#Edit bid lines.
+		#List choices
+			#Change existing bidlines.
+			#Add new bid lines.
+			#Delete bid lines.
+	
+	#Edit Employees.
+		#List choices.
+			#Change existing employee.
+			#Add new employee.
+			#Delete employee.
+		
+	#Have employee enter their name and seniority number.
+		#Check employee is next in line by seniority number.
+		#List available bid lines and employee's current bid line.
+		
+	#Have employee enter new bid line choice.
+		#If chosen bid line is available assign it to the employee and make it unavailable to others.
+		#If chosen bidline is not available have employee choose another one.
+		
