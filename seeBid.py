a3a7694513c3ec4f215324cd67d4d4249d3a3142
Micah M <mcmxl@reagan.com>
2018-01-28 19:20:13 -0700

    Update and rename seebid.py to seeBid.py
    Python 3.X compatible.

--- seeBid.py
+++ seeBid.py
@@ -1,25 +1,21 @@
+#! /usr/bin/env python
+# By Micah M. 2018
+# seeBid version 1.01
+# Python 3.6.4
+
+
 import sqlite3
 
 
-def seebid():
-    # defines variable conn.
+def seeBid():
     conn = sqlite3.connect('Bid.db')
-
-    # defines variable cursor.
     cursor = conn.cursor()
-
-    # defines variable sql as an SQLite command.
     sql = '''select * from bid'''
-
-    # defines variable results.
     results = cursor.execute(sql)
-
-    # defines variable all_employees.
     all_employees = results.fetchall()
 
-    # for-loop to print contents of variable all_employees.
     for employee in all_employees:
         print employee
 
 if __name__ == "__main__":
-    seebid()
+    seeBid()
