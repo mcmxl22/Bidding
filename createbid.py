5fdb0a22ac36399468a2a93dcfabb0d8da618ef9
Micah M <mcmxl@reagan.com>
2018-06-18 11:44:09 -0600

    minor updates

--- createbid.py
+++ createbid.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # By Micah M. 2018
-# createBid version 1.02
+# createBid version 1.02.02
 # Python 3.6.5
 
 
@@ -10,12 +10,13 @@ import sqlite3
 
 class bid():
 
-    def createbid(self):
-
-        conn = sqlite3.connect('bid.db')
-        cursor = conn.cursor()
+    def createBid(self):
 
         while True:
+
+            fileName = input('Enter file name.\n> ')
+            conn = sqlite3.connect(fileName + '.db')
+            cursor = conn.cursor()            
             Name = input('Employee\'s name: ')
             Seniority = input('Employee\'s seniority: ')
             BidLine = input('Employee\'s bid: ')
@@ -28,24 +29,35 @@ class bid():
                             'em_Seniority': Seniority,
                             'em_BidLine': BidLine})
             conn.commit()
-            cont = input('Add Another Employee? ')
+            cont = input('Add Another Employee?\n> ')
             if cont[0].lower() == 'n':
-                break
+                cursor.close()
+                raise SystemExit
         cursor.close()
-        
+
+
     def createTable(self):
-        
-        conn = sqlite3.connect('bid.db')
-        cursor = conn.cursor()        
-        print('Creating table.')
-        sql = '''create table bid (
-            Name text,
-            Seniority int,
-            Bidline int)'''
-        cursor.execute(sql)
-        print('Done!')
-        bid.createbid()
+
+        creatFile = input('Do you want to create a new bid file?\n> ')
+
+        if creatFile == 'y':
+
+            fileName = input('What should the filename be?\n> ')
+            conn = sqlite3.connect(fileName + '.db')
+            cursor = conn.cursor()        
+            print('Creating table.')
+            sql = '''create table bid (
+                Name text,
+                Seniority int,
+                Bidline int)'''
+            cursor.execute(sql)
+            print('Done!')
+            bid.createBid()
+
+        else:
+            raise SystemExit
 
 if __name__ == "__main__":
     bid = bid()
     bid.createTable()
+
