e0c8dc711d4ae510e6b485f463789943cf0a2a07
Micah M <mcmxl@reagan.com>
2018-06-19 02:35:29 -0600

    major redesign

--- deleteBid.py
+++ deleteBid.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # By Micah M. 2018
-# deleteBid version 1.02
+# deleteBid version 1.02.01
 # Python 3.6.5
 
 
@@ -10,14 +10,19 @@ import os
 def delete():
 
     fileName = input('Enter file to be deleted.\n> ')
+    if os.path.exists(fileName) is False:
+        print('File doesn\'t exist!')
+        delete() 
+    
     confirm = input('Are you sure you want to delete %s?\n> ' % fileName)
+  
     if confirm == 'y':
         os.remove(fileName)
         delete()
     else:
-        print('Invalid entry!')
         delete()
     
 
 if __name__ == "__main__":
     delete()
+
