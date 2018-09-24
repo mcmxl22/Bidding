5100b2f6cf936b09689daba6b701243da450f51b
Micah M <mcmxl@reagan.com>
2018-09-23 23:48:05 -0600

    Update createbid.py
    removed class

--- createbid.py
+++ createbid.py
@@ -1,38 +1,33 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 '''By Micah M. 2018
-   createBid version 1.2
+   createBid version 1.3
    Python 3.7'''
-
 
 import sqlite3
 
+def create_bid():
+    '''create_bid'''
+    conn = sqlite3.connect('Bid.db')
+    cursor = conn.cursor()
 
-class bid():
-
-    def create_bid(self):
-
-        conn = sqlite3.connect('Bid.db')
-        cursor = conn.cursor()
-
-        while True:
-            Name = input('Team Member\'s name: ')
-            Seniority = input('Team Member\'s seniority: ')
-            BidLine = input('Team Member\'s bid: ')
-            sql = '''insert into Bid
-                     (Name, Seniority, bid_line)
-                      values
-                     (:em_Name, :em_Seniority, :em_bid_line)'''
-            cursor.execute(sql,
-                           {'em_Name': Name,
-                            'em_Seniority': Seniority,
-                            'em_BidLine': bid_line})
-            conn.commit()
-            cont = input('Add another team member? ')
-            if cont[0].lower() == 'n':
-                cursor.close()
-                raise SystemExit
+    while True:
+        Name = input('Team Member\'s name: ')
+        Seniority = input('Team Member\'s seniority: ')
+        bid_line = input('Team Member\'s bid: ')
+        sql = '''insert into Bid
+                 (Name, Seniority, bid_line)
+                  values
+                 (:tm_Name, :tm_Seniority, :tm_bid_line)'''
+        cursor.execute(sql,
+                      {'tm_Name': Name,
+                       'tm_Seniority': Seniority,
+                       'tm_BidLine': bid_line})
+        conn.commit()
+        cont = input('Add another team member? ')
+        if cont[0].lower() == 'n':
+            cursor.close()
+            raise SystemExit
 
 
 if __name__ == "__main__":
-    bid = bid()
-    bid.create_bid()
+    create_bid()
