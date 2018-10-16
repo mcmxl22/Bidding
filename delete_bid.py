baa4ed088d02475a7c0175e3924be6c55fa6a22f
Micah M <mcmxl@reagan.com>
2018-10-16 12:06:22 -0600

    Update delete_bid.py

--- delete_bid.py
+++ delete_bid.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''By Micah M. 2018
-   delete_bid version 1.1
+   delete_bid version 1.2
    Python 3.7'''
 
 
@@ -11,25 +11,21 @@ import subprocess
 
 def delete():
     '''Delete a bid file.'''
-    file_name = input('Enter file to be deleted or press b to go back.\n> ')
-    if file_name == 'b':
-        Bid = [sys.executable, 'Bid.py']
-        subprocess.call(Bid)
+    while True:
+        file_name = input('Enter file to be deleted or press b to go back.\n> ')
+        if file_name == 'b':
+            Bid = [sys.executable, 'Bid.py']
+            subprocess.call(Bid)
     
-    if os.path.exists(file_name) is False:
-        print(f'File {file_name} doesn\'t exist!')
-        delete() 
+        if os.path.exists(file_name) is False:
+            print(f'File {file_name} doesn\'t exist!')
     
-    confirm = input(f'Are you sure you want to delete {file_name}?\n> ')
+        confirm = input(f'Are you sure you want to delete {file_name}?\n> ')
   
-    if confirm == 'y':
-        os.remove(file_name) 
-        print('Done!')
-        delete()
-    else:
-        delete()
+        if confirm == 'y':
+            os.remove(file_name) 
+            print('Done!')
     
 
 if __name__ == "__main__":
     delete()
-
