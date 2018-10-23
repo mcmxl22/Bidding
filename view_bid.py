f69cebfe80946cef58dac3b143229186fc747d4b
Micah M <mcmxl@reagan.com>
2018-10-23 12:17:48 -0600

    Update and rename see_bid.py to view_bid.py

--- view_bid.py
+++ view_bid.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python
 '''By Micah M. 2018
-   see_bid version 1.2
+   seeBid version 1.2
    Python 3.7'''
 
 
@@ -8,14 +8,15 @@ import sqlite3
 
 
 def see_bid():
-    '''view bid results'''
+    '''View a bid'''
     conn = sqlite3.connect('Bid.db')
     cursor = conn.cursor()
     sql = '''select * from bid'''
     results = cursor.execute(sql)
-    team_members = results.fetchall()
-    for team in team_members:
-        print(team)
+    all_team_members = results.fetchall()
+    for members in all_team_members:
+        print(members)
+
 
 if __name__ == "__main__":
     see_bid()
