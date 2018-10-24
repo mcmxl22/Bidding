2f45da3937720d2e80e537333358c822443ebe30
Micah M <mcmxl@reagan.com>
2018-10-24 11:53:46 -0600

    Update Bid.py

--- Bid.py
+++ Bid.py
@@ -1,102 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 '''By Micah M. 2018
-   Bid Version 1.2
+   bid version 1.3
    Python 3.7'''
 
 
-import sys
 import subprocess
+import sys
 
 
-# Create lists of all bid lines, and all team members with their
-# seniority numbers and current bid line numbers.
+def bid():
+    '''bid'''
+    create_bid = [sys.executable, 'createbid.py']
+    view_bid = [sys.executable, 'seebid.py']
+    bid_options = ('1 create bid', '2 View bid', '3 Exit')
 
-def activityChoices():
-    entry = ('\nInvalid Entry!\n')
-    actions = ['1 Create new bid',  '2 View current bid files',
-               '3 Edit bid lines', '4 Edit team members', '5 Create Lists',
-               '6 Delete a bid', '7 Exit\n']
-    print(' \n'.join(actions))
+    print('\n'.join(bid_options))
+    bid_choice = input('What do you want to do? ')
 
-    select = input('Select an option.\n> ')
+    if bid_choice == '1':
+        subprocess.run(create_bid)
 
-    if select == '1':
-        createBid = [sys.executable, 'createbid.py']
-        subprocess.call(createBid)
+    elif bid_choice == '2':
+        subprocess.run(view_bid)
 
-    # View available bid files.
-    elif select == '2':
-        seeBid = [sys.executable, 'seeBid.py']
-        subprocess.call(seeBid)
-
-    # Edit bid lines.
-    elif select == '3':
-
-        editActions = ['1 Change existing bidlines.',
-                       '2 Add new bid lines.',
-                       '3 Delete bid lines.', '4 Back']
-
-        print(' \n'.join(editActions))
-        select = input('Select an option.\n> ')
-
-        if select == '1':
-            print('Under development!')
-            activityChoices()
-
-        elif select == '2':
-            print('Under development!')
-            activityChoices()
-
-        elif select == '3':
-            print('Under development!')
-            activityChoices()
-
-        if select == '4':
-            activityChoices()
-
-    # Edit team members.
-    elif select == '4':
-        editChoices = ['1 Change existing team members.',
-                       '2 Add new team members.',
-                       '3 Remove team members.', '4 Back']
-
-        print(' \n'.join(editChoices))
-        select = input('Select an option.\n> ')
-
-        if select == '1':
-            print('Under development!')
-            activityChoices()
-
-        elif select == '2':
-            print('Under development!')
-            activityChoices()
-
-        elif select == '3':
-            print('Under development!')
-            activityChoices()
-
-        elif select == '4':
-            activityChoices()
-
-        else:
-            print(entry)
-            activityChoices()
-
-    elif select == '5':
-        print('\nunder development!\n')
-        activityChoices()
-
-    elif select == '6':
-        deleteBid = [sys.executable, 'deleteBid.py']
-        subprocess.call(deleteBid)
-
-    elif select == '7':
+    elif bid_choice == '3':
         raise SystemExit
 
     else:
-        print(entry)
-        activityChoices()
+        print('Invalid Answer!')
+        bid()
 
 
 if __name__ == "__main__":
-    activityChoices()
+    bid()
