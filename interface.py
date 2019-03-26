96caffa6877120bd5f8393a0e0b00515f5665187
Micah M <mcmxl@reagan.com>
2019-03-26 13:56:54 -0600

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
-"""By Micah M. 2018
-   Interface version 1.2
-   Python 3.7.1"""
+#!/bin/python3
+"""Interface version 1.2
+   Python 3.7.2"""
 
 import sqlite3
 from create_bid import create_bid
