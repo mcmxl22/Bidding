5619a38bcf3005735ea38826a2dc40517f64897c
Micah M <mcmxl@reagan.com>
2020-03-05 23:25:24 -0700

    Update view_bid.py

--- view_bid.py
+++ view_bid.py
@@ -1,6 +1,8 @@
 #!/bin/python3
-"""view_bid version 1.3
-   Python 3.7.2"""
+"""
+view_bid version 1.4
+Python 3.7
+"""
 
 
 import sqlite3
@@ -8,9 +10,9 @@ import sqlite3
 
 def view_bid():
     """View a bid."""
-    conn = sqlite3.connect('Bid.db')
+    conn = sqlite3.connect("Bid.db")
     cursor = conn.cursor()
-    sql = '''select * from bid'''
+    sql = """select * from bid"""
     results = cursor.execute(sql)
     all_team_members = results.fetchall()
     for members in all_team_members:
