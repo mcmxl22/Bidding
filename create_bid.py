6c3e05f545164f92ecb26167346354e4a436aace
Micah M <mcmxl@reagan.com>
2020-03-05 23:23:00 -0700

    Update create_bid.py

--- create_bid.py
+++ create_bid.py
@@ -1,6 +1,8 @@
 #!/bin/python3
-"""create_bid version 1.4
-   Python 3.7."""
+"""
+create_bid version 1.5
+Python 3.7
+"""
 
 
 import sqlite3
@@ -8,26 +10,25 @@ import sqlite3
 
 def create_bid():
     """create_bid"""
-    conn = sqlite3.connect('Bid.db')
+    conn = sqlite3.connect("Bid.db")
     cursor = conn.cursor()
 
     while True:
-        name = input('Name: ')
-        seniority = input('Seniority: ')
-        bid_line = input('Desired bid: ')
-        sql = '''insert into Bid
+        name = input("Name: ")
+        seniority = input("Seniority: ")
+        bid_line = input("Desired bid: ")
+        sql = """insert into Bid
                  (Name, Seniority, Bidline)
                   values
-                 (:tm_Name, :tm_Seniority, :tm_Bidline)'''
-        cursor.execute(sql,
-                       {'tm_Name': name,
-                       'tm_Seniority': seniority,
-                       'tm_Bidline': bid_line})
+                 (:tm_Name, :tm_Seniority, :tm_Bidline)"""
+        cursor.execute(
+            sql, {"tm_Name": name, "tm_Seniority": seniority, "tm_Bidline": bid_line}
+        )
         conn.commit()
-        add_member = input('Add another team member? ')
-        if add_member[0].lower() in 'n':
+        add_member = input("Add another team member? ")
+        if add_member[0].lower() in "n":
             cursor.close()
-            raise SystemExit
+            exit(0)
 
 
 if __name__ == "__main__":
